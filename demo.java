e493e927b4868f9ca27df53ba286af41cf1a9bd3
devopstraining611-282 <devopstraining611-282@gmail.com>
2023-11-17 06:00:53 +0000

    some function created

--- demo.java
+++ demo.java
@@ -2,5 +2,8 @@ public static void main (){
 	system.out.print("Welcome to My App");
 }
 void newfunction(){
-	system.out.print("Some New function is added""origi);
+	system.out.print("Some New function is added");
+}
+void somefunction(){
+	system.out.println("Some function added");
 }
